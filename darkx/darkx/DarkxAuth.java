28057661a490a20f91927cf0978923c91e7b2790
DarkxKrist <d@rk.xfake>
2013-01-18 16:24:40 +0200

    Fixed earlier commits side-effects, whooops.

--- darkx/darkx/DarkxAuth.java
+++ darkx/darkx/DarkxAuth.java
@@ -10,6 +10,8 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import darkx.darkxauth.handlers.PlayerTracker;
 import darkx.darkxcore.lib.Log;
 import darkx.darkxcore.lib.Reference;
@@ -32,9 +34,9 @@ public class DarkxAuth {
 		Log.log("DarkxAuth loaded.");
 	}
 	
+	@SideOnly(Side.SERVER)
 	@Init
 	public void load(FMLInitializationEvent event) {
-		proxy.registerRenderers();
 		GameRegistry.registerPlayerTracker(new PlayerTracker());
 	}
 	
--- darkx/darkx/darkxauth/handlers/PlayerTracker.java
+++ darkx/darkx/darkxauth/handlers/PlayerTracker.java
@@ -1,19 +1,25 @@
 package darkx.darkxauth.handlers;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.server.integrated.IntegratedServer;
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.IPlayerTracker;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import cpw.mods.fml.server.FMLServerHandler;
 import darkx.DarkxCore;
 import darkx.darkxauth.network.PacketAuthQ;
 import darkx.darkxcore.lib.PacketIds;
 
 public class PlayerTracker implements IPlayerTracker {
 	
-
+	@SideOnly(Side.SERVER)
 	@Override
 	public void onPlayerLogin(EntityPlayer player) {
-		if (FMLClientHandler.instance().getServer().isDedicatedServer())
-			DarkxCore.proxy.sendToPlayer(player, new PacketAuthQ(PacketIds.AUTH_Q));	
+			if (FMLServerHandler.instance().getServer().isDedicatedServer())
+				DarkxCore.proxy.sendToPlayer(player, new PacketAuthQ(PacketIds.AUTH_Q));	
 	}
 
 	@Override
